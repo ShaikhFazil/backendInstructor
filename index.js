7e7824dd85dbe91861bc1d23a9e65f6ac3e0bb16
ShaikhFazil <shaikhfazil180@gmail.com>
2023-12-24 19:44:08 +0530

    first deployment

--- index.js
+++ index.js
@@ -23,4 +23,8 @@ app.use('/api/v1',require('./routes/contact'))
 
 
 const PORT = process.env.PORT;
+
 app.listen(PORT, () => console.log(`server is connected on port no ${PORT}`));
+app.get("/", (req, res) => {
+    res.send("Api EndPoint Working Fine...");
+  });
